f3f9783ed74802989e37dcfe6c6f6eec56ef156f
hayghlander <hayghlanderuitec@gmail.com>
2025-01-21 11:59:35 -0300

    Tela de login e criar conta.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Path } from './../../node_modules/typescript/lib/typescript.d';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent,
+  }
+];
+ 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-TESTE
