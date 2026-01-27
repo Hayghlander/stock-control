22858547e34e5bda4d43c69cb26097011a82edec
hayghlander <hayghlanderuitec@gmail.com>
2026-01-27 18:30:21 -0300

    Criando shared module componente de Toolbar e Implementando funcionalidade de logout

---
+++ src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
@@ -0,0 +1,17 @@
+import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+
+@Component({
+  selector: 'app-toolbar-navigation',
+  templateUrl: './toolbar-navigation.component.html',
+  styleUrls: [],
+})
+export class ToolbarNavigationComponent {
+  constructor(private cookie: CookieService, private router: Router) {}
+
+  handleLogout(): void{
+    this.cookie.delete('USER_INFO')
+    void this.router.navigate(['/home']);
+  }
+}
